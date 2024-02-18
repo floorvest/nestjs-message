737567d6be6c69604374d93d5d21132e2331e71e
Patranto Bagus Prabowo <patranto.prabowo@gmail.com>
2024-02-18 17:56:35 +0700

    setup config, database and moment timezone

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersModule } from './users/users.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { MomentModule } from '@ccmos/nestjs-moment';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    MomentModule.forRoot({
+      tz: 'Asia/Jakarta'
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+    }),
+    ScheduleModule.forRoot(),
+    UsersModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
